7dedd5b419beaefe3019cb6bf535ac12de43facd
YURNE11 <103185020+YURNE11@users.noreply.github.com>
2022-04-21 15:07:02 +0200

    Add files via upload

---
+++ pratique DOM/index.js
@@ -0,0 +1,16 @@
+var title =document.querySelector('h1')
+title.style.color = 'red';
+title.style.textDecoration = 'underline';
+title.style.margin ='20px 20px 10px 10px'
+
+var ul = document.querySelectorAll('ul');
+
+var buttons = document.querySelectorAll('button');
+
+    for(var button of buttons){
+    button.style.marginLeft = '12px';
+    }
+
+    function foo(){
+        var 
+    } 
